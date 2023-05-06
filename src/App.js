a6c2b5abeac279de48b1c2ef5905b157eb45affc
Dwayne Jones <dwaynejones13123@gmail.com>
2023-05-06 07:29:17 -0400

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
 
 function App() {
+  const URL = "https://portfolio-backend-a.herokuapp.com/"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <Routes>
+     <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/projects" element={<Projects URL={URL}/>}/>
+        <Route exact path="/about" element={<About URL={URL}/>}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+function Home(props) {
+    return(
+       
+        <div>
+            <h1>Home</h1> 
+        <a href='https://docs.google.com/document/d/1SBA40h_-nO8MHYCY76N1scf1Sr52f6_Z/edit?usp=sharing&ouid=105174522351646214768&rtpof=true&sd=true'>
+        <button>Resume</button>
+        </a>
+        </div>
+    )
+    
+    
+  }
+  
+  export default Home;
+  
